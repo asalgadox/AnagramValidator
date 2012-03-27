35ce5d3d19624a97fd4b66a0c5d824e09876d723
Anthony Salgado <asalgadox@gmail.com>
2012-03-26 22:52:47 -0700

    using defaultdict instead of dict, makes initialization easier, cleans up code

--- AnagramValidator.py
+++ AnagramValidator.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python
 import re
 import sys
-
+from collections import defaultdict
+"""
+Class definition for AnagramValidator
+"""
 class AnagramValidator(object):
 
   def __init__(self):
-    self.__dict = {}
+    """
+    Maps <sorted(word), anagrams of word that are valid english>
+    """
+    self.__dict = defaultdict(lambda : set())
     fileHandle = open('words.txt')
     for line in fileHandle:
-      m = re.search('\w+',line)
-      if m is not None:
-        key = ''.join(sorted(m.group()))
-        if key in self.__dict: self.__dict[key].add(m.group())
-        else: self.__dict[key] = set([m.group()])
+      m = re.search('[a-z]+',line)	# parse each line for the word
+      if m is not None: # this might be None, convention of re.search
+        key = ''.join(sorted(m.group())) # sort the word and use as key
+        self.__dict[key].add(m.group()) # defaultdict lets us get away without initializing set()
     fileHandle.close()
 
   def validAnagrams(self,w):
-    key = ''.join(sorted(w))
-    return self.__dict.get(key)
+    key = ''.join(sorted(w)) # the key is a sorted sequence of characters, so sort the sequence
+    results = self.__dict[key] # return valid anagrams from pre-processor method
+    return results
 
 if __name__ == '__main__':
 	v = AnagramValidator()
