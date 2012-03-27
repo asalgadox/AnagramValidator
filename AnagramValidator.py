53fa56fcf0a5bc05f000dd2d5e837ab00194b96f
Anthony Salgado <asalgadox@gmail.com>
2012-03-26 22:53:58 -0700

    fixing case sensitivity

--- AnagramValidator.py
+++ AnagramValidator.py
@@ -14,14 +14,14 @@ class AnagramValidator(object):
     self.__dict = defaultdict(lambda : set())
     fileHandle = open('words.txt')
     for line in fileHandle:
-      m = re.search('[a-z]+',line)	# parse each line for the word
+      m = re.search('[a-z]+',line.lower())	# parse each line for the word
       if m is not None: # this might be None, convention of re.search
         key = ''.join(sorted(m.group())) # sort the word and use as key
         self.__dict[key].add(m.group()) # defaultdict lets us get away without initializing set()
     fileHandle.close()
 
   def validAnagrams(self,w):
-    key = ''.join(sorted(w)) # the key is a sorted sequence of characters, so sort the sequence
+    key = ''.join(sorted(w.lower())) # the key is a sorted sequence of characters, so sort the sequence
     results = self.__dict[key] # return valid anagrams from pre-processor method
     return results
 
